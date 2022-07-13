19b7bae808caf8f096943c289bc9a3dfe90efc45
jonnyhoudini <72469020+jonnyhoudini@users.noreply.github.com>
2022-07-13 22:17:00 +0100

    added carousel code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import NavbarComponent from './components/NavbarComponent';
 import Home from './containers/Home';
+import About from './containers/About';
+import Skills from './containers/Skills';
+import Projects from './containers/Projects';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <NavbarComponent />
-      <Home />
+      <Router>
+        <NavbarComponent />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+
     </>
   );
 }
---
+++ src/containers/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ProjectCarousel from '../components/ProjectCarousel'
+
+const Projects = () => {
+    return (
+        <>
+            <main>
+                <div className="transparent-bg">
+                    <ProjectCarousel />
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Projects
