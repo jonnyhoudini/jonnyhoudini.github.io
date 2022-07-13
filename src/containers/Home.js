24d8e7466b03018ad256206983025692162d2c15
jonnyhoudini <72469020+jonnyhoudini@users.noreply.github.com>
2022-07-13 16:39:56 +0100

    animated text added

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,7 @@ import "../static/Home.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import TypeAnimation from 'react-type-animation';
 
 const Home = () => {
 
@@ -15,7 +16,14 @@ const Home = () => {
             <main>
                 <div className="transparent-bg">
                     <h1 className="header">JONNY HOUDIN MCAVEETY</h1>
-                    <h2 className="intro">I'm a junior developer</h2>
+                    <div className="intro">
+                        <TypeAnimation
+                            cursor={true}
+                            sequence={["I'm a junior developer", 1000, '']}
+                            wrapper="h2"
+                            repeat={3}
+                        />
+                    </div>
                     <div className="contact">
                         <a className="icon" href="https://www.linkedin.com/in/jonathan-houdin-mcaveety-392a1153/">{linkedIn}</a>
                         <a className="icon" href="https://github.com/jonnyhoudini">{github}</a>
@@ -23,6 +31,7 @@ const Home = () => {
                     </div>
                 </div>
             </main>
+
 
         </>
     )
