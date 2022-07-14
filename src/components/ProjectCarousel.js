a1fc347307af25121765dc81bed6d5010aa0340b
jonnyhoudini <72469020+jonnyhoudini@users.noreply.github.com>
2022-07-14 12:19:09 +0100

    added photos and captions to carousel

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -1,41 +1,49 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
+
 const ProjectCarousel = () => {
+
+    const captionStyles = {
+        color: 'rgba(62, 212, 176)',
+        backgroundColor: 'black',
+        padding: '2px',
+    }
+
     return (
-        <Carousel>
+        <Carousel variant="dark" className="carousel">
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://placedog.net/800/"
+                    src="https://i.ibb.co/0KVbf7R/marko.png"
                     alt="First slide"
                 />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
+                <Carousel.Caption style={captionStyles}>
+                    <h3>Marko Expenses Tracker</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://placedog.net/800/"
+                    src="https://i.ibb.co/DKWpmNh/dinosaur.png"
                     alt="Second slide"
                 />
 
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
+                <Carousel.Caption style={captionStyles}>
+                    <h3>BBC Educational App</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://placedog.net/800/"
+                    src="https://i.ibb.co/TRSyBzt/rps.png"
                     alt="Third slide"
                 />
 
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
+                <Carousel.Caption style={captionStyles}>
+                    <h3>Rock, Paper, Scissors Game</h3>
                     <p>
                         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                     </p>
--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ProjectCarousel from '../components/ProjectCarousel'
+import '../static/Projects.css';
 
 const Projects = () => {
+
+    const h1Styles = {
+        fontWeight: 900,
+        marginTop: '30px',
+    }
     return (
         <>
             <main>
                 <div className="transparent-bg">
+                    <h1 style={h1Styles}>My Projects</h1>
                     <ProjectCarousel />
                 </div>
             </main>
